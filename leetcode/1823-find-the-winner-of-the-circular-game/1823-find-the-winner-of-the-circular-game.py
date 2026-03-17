c9a97a992bfb122eeaca691726ab9aa0fc509c3a
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-03-17 18:43:16 +0300

    Add solution for Find the Winner of the Circular Game

--- leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ leetcode/1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -2,15 +2,18 @@ class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
         arr = [i for i in range(1,n+1)]
-        i = 0
 
-        while  n > 1:
-            i = (i + k - 1) % n
+        def circulaArr(a,b,i=0):
+            if a == 1:
+                return arr[0]
+            
+            i = (i + b - 1) % a
             arr.pop(i)
+            a -= 1
 
-            n -= 1
+            return circulaArr(a,b,i)
         
-        return arr[0]
+        return circulaArr(n,k)
 
 
 
