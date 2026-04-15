d02d0d05906a9dbc8a337fa25df9f6dbfc3bf3c8
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-04-15 19:17:33 +0300

    Add solution for Set Mismatch

--- leetcode/0645-set-mismatch/0645-set-mismatch.py
+++ leetcode/0645-set-mismatch/0645-set-mismatch.py
@@ -2,20 +2,35 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         ans = []
 
-        cnt = collections.Counter(nums)
+        # cnt = collections.Counter(nums)
 
-        cnt = sorted(cnt.items(),key = lambda x:x[1],reverse=True)
-        ans.append(cnt[0][0])
+        # cnt = sorted(cnt.items(),key = lambda x:x[1],reverse=True)
+        # ans.append(cnt[0][0])
 
         set_nums = set(nums)
-        # print(len(nums))
+        # # print(len(nums))
+        cnt = [0] * len(nums) 
+        miss = 0
+        duplicat = 0
+
 
         for i in range(1,len(nums)+1):
             if i not in set_nums:
                 # print(i)
-                ans.append(i)
+                miss = i
                 break
         
+        for n in nums:
+
+            cnt[n-1] += 1
+
+            if cnt[n-1] == 2:
+                duplicat = n
+
+        ans.append(duplicat)
+        ans.append(miss)
+
+    
         return ans
 
 
