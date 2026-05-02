56a9c062ecb93ae5c261a09257808963eaadec7e
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-05-02 22:29:08 +0300

    Time: 232 ms (88.27%), Space: 21.6 MB (72.33%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,13 +4,13 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
 
-        visited = [[False] * cols for _ in range(rows)]
+        # visited = [[False] * cols for _ in range(rows)]
 
         def dfs(i,j):
-            if i < 0 or i == rows  or j < 0 or j == cols or visited[i][j] or grid[i][j] == '0':
+            if i < 0 or i == rows  or j < 0 or j == cols or grid[i][j] == '0':
                 return
 
-            visited[i][j] = True
+            grid[i][j] = '0'
             dfs(i+1,j)
             dfs(i-1,j)
             dfs(i,j+1)
@@ -20,7 +20,7 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1' and not visited[r][c]:
+                if grid[r][c] == '1':
                     dfs(r,c)
                     count += 1
 
