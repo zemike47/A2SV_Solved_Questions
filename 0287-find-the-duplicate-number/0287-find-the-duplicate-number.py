e8e7d5f94fb0cecdd5260d9f9f99c75c10698760
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-06-23 22:34:02 +0300

    Time: 63 ms (14.8%), Space: 33.8 MB (14.13%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,19 +1,31 @@
 class Solution:
     def findDuplicate(self, nums):
-        slow = nums[0]
-        fast = nums[0]
+        # slow = nums[0]
+        # fast = nums[0]
 
-        # Step 1: detect cycle
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break
+        # # Step 1: detect cycle
+        # while True:
+        #     slow = nums[slow]
+        #     fast = nums[nums[fast]]
+        #     if slow == fast:
+        #         break
 
-        # Step 2: find entrance
-        slow = nums[0]
-        while slow != fast:
-            slow = nums[slow]
-            fast = nums[fast]
+        # # Step 2: find entrance
+        # slow = nums[0]
+        # while slow != fast:
+        #     slow = nums[slow]
+        #     fast = nums[fast]
 
-        return slow
+        # return slow
+
+        dupilicate = -1
+
+        for num in nums:
+            if nums[abs(num) - 1] < 0:
+                dupilicate = abs(num)
+            
+            else:
+                nums[abs(num)-1] *= -1
+        
+        return dupilicate
+
