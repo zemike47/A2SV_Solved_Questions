69771d5542a3705015299ff2037357f5b505207f
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-08 11:33:05 +0300

    Add solution for Word Search II

---
+++ leetcode/0212-word-search-ii/0212-word-search-ii.py
@@ -0,0 +1,91 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+
+class Solution:
+
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+
+        prefixes = set()
+
+        for word in words:
+            for i in range(1,len(word)+1):
+                pre = word[:i]
+                prefixes.add(pre)
+
+        word_sets = set(words)
+        
+        # root = TreeNode()
+
+        # for word in words:
+        #     node = root()
+
+        #     for char in words:
+
+        #         if char not in node.children:
+        #             node.children[char] = TreeNode()
+
+        #         node = node.children[char]
+
+        #         node.word = word
+        
+        result = set()
+
+     
+        def dfs(r,c,current):
+
+            if r < 0 or c < 0 or r >= len(board) or c >= len(board[0]):
+                return
+
+            
+
+            if board[r][c] == "#":
+                return
+            
+            current += board[r][c]
+
+            if current not in prefixes:
+                return 
+
+            if current in word_sets:
+                result.add(current)
+                
+
+
+            # if node not in node.children:
+            #     return
+
+            # node = node.children[char]
+
+            # if node.word:
+            #     result.append(node)
+            #     node.word = None
+
+            char = board[r][c]
+            board[r][c] = "#"
+
+            # dfs(r+1,c,node)
+            # dfs(r-1,c,node)
+            # dfs(r,c-1,node)
+            # dfs(r,c+1,node)
+
+            dfs(r+1,c,current)
+            dfs(r-1,c,current)
+            dfs(r,c-1,current)
+            dfs(r,c+1,current)
+
+
+            board[r][c] = char
+        
+
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                dfs(r,c,"")
+                # dfs(r,c,root)
+
+        # return result
+
+        return list(result)
+
