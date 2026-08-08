da0a4baba60c2076f3c35cf3b025874b216e35bf
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-08 12:40:20 +0300

    Add solution for Number of Islands

--- leetcode/0200-number-of-islands/0200-number-of-islands.py
+++ leetcode/0200-number-of-islands/0200-number-of-islands.py
@@ -1,30 +1,69 @@
 class Solution:
     def numIslands(self, grid):
 
+
+        # def dfs(r,c):
+        #     if r >= len(grid) or c >= len(grid[0]) or r < 0 or c < 0 or grid[r][c] == "0":
+        #         return 
+
+        #     grid[r][c] = "0"
+            
+        #     dfs(r+1,c)
+        #     dfs(r-1,c)
+        #     dfs(r,c+1)
+        #     dfs(r,c-1)
+
+        # count = 0
+
+        # for r in range(len(grid)):
+        #     for c in range(len(grid[0])):
+
+        #         if grid[r][c] == "1":
+        #             dfs(r,c)
+        #             count += 1
+
+        # return count
+
+
         rows = len(grid)
         cols = len(grid[0])
 
-        # visited = [[False] * cols for _ in range(rows)]
+        directions = [(1,0),(-1,0),(0,1),(0,-1)]
 
-        def dfs(i,j):
-            if i < 0 or i == rows  or j < 0 or j == cols or grid[i][j] == '0':
-                return
+        islands = 0
 
-            grid[i][j] = '0'
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
-
-        count = 0
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
-                    dfs(r,c)
-                    count += 1
 
-        return count
+                if grid[r][c] == "0":
+                    continue
+
+                queue = deque([(r,c)])
+                grid[r][c] = "0"
+
+                islands += 1
+
+                while queue:
+                    
+                    row,col = queue.popleft()
+
+                    for dr,dc in directions:
+                        nr = row + dr
+                        nc = col + dc 
+
+                        if 0 <= nr < rows and 0 <= nc < cols:
+
+                            if grid[nr][nc] == "1":
+                                queue.append((nr,nc))
+                                grid[nr][nc] = "0"
+
+        return islands
+                            
+
+
+
+
 
 
         
