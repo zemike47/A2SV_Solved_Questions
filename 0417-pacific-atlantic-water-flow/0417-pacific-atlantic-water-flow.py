8a89a2b5b92c13c8e45abacd76375efcdb02ff83
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-10 15:17:50 +0300

    Time: 35 ms (38.34%), Space: 22.4 MB (25.29%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,41 +1,45 @@
 class Solution:
     def pacificAtlantic(self, heights):
-        if not heights:
-            return []
 
-        rows, cols = len(heights), len(heights[0])
+        rows = len(heights)
+        cols = len(heights[0])
 
         pacific = set()
         atlantic = set()
 
-        def dfs(r, c, visited):
-            visited.add((r, c))
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
 
-            directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        def dfs(r,c,visited):
+            visited.add((r,c))
 
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
+            for dr , dc in directions:
+                nr = r + dr
+                nc = c + dc
+            
 
-                if (0 <= nr < rows and 0 <= nc < cols and
-                    (nr, nc) not in visited and
-                    heights[nr][nc] >= heights[r][c]):
-                    
-                    dfs(nr, nc, visited)
+                if 0 <= nr < rows and 0 <= nc < cols and  (nr,nc) not in visited and heights[nr][nc] >= heights[r][c]:
+                    dfs(nr,nc,visited)
+            
+                
+
+        for r in range(rows):
+            dfs(r,0,pacific)
 
         for c in range(cols):
-            dfs(0, c, pacific)
-        for r in range(rows):
-            dfs(r, 0, pacific)
+            dfs(0,c,pacific)
 
         for c in range(cols):
-            dfs(rows - 1, c, atlantic)
+            dfs(rows - 1,c,atlantic)
+        
         for r in range(rows):
-            dfs(r, cols - 1, atlantic)
+            dfs(r,cols-1,atlantic)
 
         result = []
+
         for r in range(rows):
             for c in range(cols):
-                if (r, c) in pacific and (r, c) in atlantic:
-                    result.append([r, c])
+                if (r,c) in pacific and (r,c) in atlantic:
+                    result.append([r,c])
 
         return result
+
