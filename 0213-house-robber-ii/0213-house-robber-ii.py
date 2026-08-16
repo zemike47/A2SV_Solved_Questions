ee499b19c2f019157699cbcdf276c04adfee5ed6
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-16 11:40:13 +0300

    Time: 0 ms (100%), Space: 19.3 MB (68.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,29 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
-        # n = len(nums)
+        n = len(nums)
 
-        def robLinear(nums):
-            n = len(nums)
-            # nonlocal n
-        
-
-            if n == 1:
-                return nums[0]
-            
-           
-
-            prev2 = nums[0]
-            prev1 = max(nums[0], nums[1])
-
-            for i in range(2, n):
-                curr = max(prev1, nums[i] + prev2)
-                prev2 = prev1
-                prev1 = curr
-
-            return prev1
-        
-        if len(nums) == 1:
+        if n == 1:
             return nums[0]
         
-        return max(robLinear(nums[:len(nums)-1]),robLinear(nums[1:]))
+        def dynammicP(nums):
+            prev1 = 0
+            prev2 = 0
+
+            for money in nums:
+                curr = max(prev1,money + prev2)
+
+                prev2 = prev1
+                prev1 = curr
+            
+            return prev1
+
+        
+
+        case1 = dynammicP(nums[1:])
+        case2 = dynammicP(nums[:n-1])
+
+        return max(case1,case2)
+
+
+
+        
