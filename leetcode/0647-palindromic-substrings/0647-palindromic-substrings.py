5a0695a2c1322161c206083f0b1f95b8260e5ca1
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-18 12:01:39 +0300

    Add solution for Palindromic Substrings

---
+++ leetcode/0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,67 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+    #     class Solution:
+    # def longestPalindrome(self, s: str) -> str:
+
+        # n = len(s)
+        # dp = [[False]*n for _ in range(n)]
+
+        # maxLen = 1
+        # resIndex = 0
+
+        # for i in range(n-1,-1,-1):
+        #     for j in range(i,n):
+
+        #         if s[i] == s[j] and (j-i <= 2 or dp[i+1][j-1] ) :
+
+        #             dp[i][j] = True
+
+        #             if maxLen < j - i + 1:
+        #                 maxLen = j - i + 1
+        #                 resIndex = i
+
+        # return s[resIndex:resIndex+maxLen]
+
+        # resIndex = 0 
+        # max_length = 0
+        count = 0
+
+        for i in range(len(s)):
+            # odd length
+            l,r = i,i
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+
+                # if r - l + 1 > max_length:
+                #     max_length = r - l + 1
+                #     resIndex = l
+                count += 1
+
+                l -= 1
+                r += 1
+        
+            # even length
+            l,r = i,i+1
+
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+
+                # if  r - l + 1 > max_length:
+                #     max_length = r - l + 1
+                #     resIndex = l
+                count += 1
+
+
+                l -= 1
+                r += 1
+
+        # return s[resIndex:resIndex+max_length]
+        return count
+            
+
+
+
+
+
+
+
+        
