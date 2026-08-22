feb195fa0ae0e9651fc350a85d99d045f1aeb261
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-22 20:59:28 +0300

    Add solution for Merge Intervals

---
+++ leetcode/0056-merge-intervals/0056-merge-intervals.py
@@ -0,0 +1,16 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key =lambda x:x[0])
+        
+        result = []
+
+        for interval in intervals:
+            if not result or interval[0] > result[-1][1]:
+                result.append(interval)
+            else:
+                result[-1][1] = max(interval[1],result[-1][1])
+
+        
+        return result
+            
+        
