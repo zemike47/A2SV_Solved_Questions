e1e239a70003195b6fd0c80e641b97a3a8b731d4
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-24 15:31:12 +0300

    Add solution for Pow(x, n)

--- leetcode/0050-powx-n/0050-powx-n.py
+++ leetcode/0050-powx-n/0050-powx-n.py
@@ -1,23 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        def recurssionPow(a,b):
-
-            if b == 0:
-                return 1
-
-            value = recurssionPow(a,b//2)
-
-            if b % 2 == 0:
-                return value * value
-            else:
-                return value * value * a
 
         if n < 0:
-            return 1 / recurssionPow(x,abs(n))
-        
-        return recurssionPow(x,n)
+            x = 1 / x
+            n = -n
 
- 
+        result = 1
 
+        while n > 0:
+            if n % 2 == 1:
+                result *= x
 
-        
+            x *= x
+            n //= 2
+
+        return result
+
