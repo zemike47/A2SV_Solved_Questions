8cf69d6cfe2fd09c77fa9056cc4b9c2577d16819
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-26 11:33:42 +0300

    Add solution for Reverse Bits

---
+++ leetcode/0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+
+        for _ in range(32):
+            bit = n & 1
+            result = (result << 1) | bit
+            n = n >> 1
+
+        return result 
