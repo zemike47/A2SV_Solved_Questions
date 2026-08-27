661e570ddb90bc8cf073183b522a7ba69f887c5a
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-27 08:26:48 +0300

    Time: 43 ms (5.24%), Space: 22.7 MB (80.43%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,28 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        c = Counter(nums)
 
-        arr = c.most_common(k)
+        hash_map = Counter(nums)
         ans = []
 
+        while k:
+            max_freq = float("-inf")
+            KEY = None
 
-
-        for i in range(len(arr)):
-            ans.append(arr[i][0])
-
+            for num,freq in hash_map.items():
+                if freq > max_freq:
+                    max_freq = freq
+                    KEY = num
+           
+            
+            ans.append(KEY)
+            del hash_map[KEY]
+            k -= 1
+        
         return ans
+            
 
+            
+
+
+
+   
