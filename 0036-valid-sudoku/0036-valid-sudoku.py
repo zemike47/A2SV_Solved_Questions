b1c5d70a560ecd60e723b8a165605b1b3ab1c1e0
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-08-27 14:08:26 +0300

    Time: 0 ms (100%), Space: 19.3 MB (34.97%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,28 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        grid = collections.defaultdict(set) # (r // 3, c//3)
-
+        rows = defaultdict(set)
+        cols = defaultdict(set)
+        three_three = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-
-
-                if board[r][c] in cols[c] or board[r][c] in rows[r] or board[r][c] in grid[(r//3,c//3)]:
+                
+                R, C = r // 3, c // 3
+                
+                if board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in three_three[(R,C)]:
                     return False
                 
-                cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
-                grid[(r // 3 , c // 3)].add(board[r][c])
-
+                cols[c].add(board[r][c])
+                three_three[(R,C)].add(board[r][c])
+        
         return True
 
 
-                
+
+            
 
 
-                
 
