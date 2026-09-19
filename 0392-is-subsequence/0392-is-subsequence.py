651ffb555b05c46efab3be74ba3cf43052700bdc
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-09-20 00:30:51 +0300

    Time: 0 ms (100%), Space: 19.6 MB (7.89%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,10 +1,38 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        i = 0
+        positions = {}
 
-        for j in range(len(t)):
-            if i < len(s) and s[i] == t[j]:
-                i += 1
+        for i, char in enumerate(t):
+            if char not in positions:
+                positions[char] = []
+            positions[char].append(i)
+        
+
+        current = -1
+
+        for char in s:
+            if char not in positions:
+                return False
             
-        return i == len(s)
+            arr = positions[char]
+
+            left = 0
+            right = len(arr)
+
+            while left < right:
+                mid = (left+right) // 2
+
+                if arr[mid] <= current:
+                    left = mid + 1
+                
+                else:
+                    right = mid
+
+            
+            if left == len(arr):
+                return False
+            
+            current = arr[left]
+        
+        return True
 
