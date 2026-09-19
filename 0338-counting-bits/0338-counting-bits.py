2878b0cf3dbbf5b4d6f691e211cd5bdbeaa1afaa
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-09-19 20:26:22 +0300

    Time: 3 ms (95.47%), Space: 20.3 MB (38.79%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,7 +3,7 @@ class Solution:
         dp = [0] * (n + 1)
         
         for i in range(n+1):
-            dp[i] = dp[i//2] + i % 2
+            dp[i] = dp[i>>1] + (i & 1)
             
         
         return dp 
