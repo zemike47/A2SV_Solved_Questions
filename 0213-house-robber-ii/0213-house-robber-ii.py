3a0e65a94ae4d900258fcf8327688fd75076b677
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-09-20 16:52:38 +0300

    Time: 0 ms (100%), Space: 19.2 MB (69.74%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,29 +1,35 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-
-        if n == 1:
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) == 1:
             return nums[0]
-        
-        def dynammicP(nums):
-            prev1 = 0
-            prev2 = 0
+         
 
-            for money in nums:
-                curr = max(prev1,money + prev2)
+        def rob(houses):
+            n = len(houses)
+
+
+            if n == 1:
+                return houses[0]
+            
+
+            prev2 = 0
+            prev1 = houses[0]
+            
+            for i in range(2,n+1):
+                skip = prev1
+                take = houses[i-1] + prev2
+
+                current = max(take,skip)
 
                 prev2 = prev1
-                prev1 = curr
+                prev1 = current
+            
             
             return prev1
-
         
-
-        case1 = dynammicP(nums[1:])
-        case2 = dynammicP(nums[:n-1])
+        case1 = rob(nums[1:])
+        case2 = rob(nums[:-1])
 
         return max(case1,case2)
 
 
-
-        
