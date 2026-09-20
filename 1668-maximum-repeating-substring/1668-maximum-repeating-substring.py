013125570e311a2f5b6e9f774ae1b69f895bd469
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-09-20 10:42:04 +0300

    Time: 0 ms (100%), Space: 19.1 MB (87.39%) - LeetHub

---
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        k = 0
+        repeated = word
+
+        while repeated in sequence:
+            k += 1
+            repeated += word
+
+        return k
