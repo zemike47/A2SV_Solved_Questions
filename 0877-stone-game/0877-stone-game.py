920c244c7e31de3df7a291fb110505e4b624edd9
Mikiyas Alemayehu <153812380+zemike47@users.noreply.github.com>
2026-09-24 09:16:31 +0300

    Time: 459 ms (5.32%), Space: 139.2 MB (5.08%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,28 @@
 class Solution:
     def stoneGame(self, piles: list[int]) -> bool:
-        return True
+        dp = {}
+
+        def dfs(l,r):
+            if l > r:
+                return 0
+            
+            if (l,r) in dp:
+                return dp[(l,r)]
+            
+            alice_turn  = (r - l + 1) % 2 == 0
+
+            left = piles[l] if alice_turn else 0
+            right = piles[r] if alice_turn else 0
+
+            dp[(l,r)] = max(dfs(l + 1,r) + left,dfs(l,r-1) + right)
+
+            return dp[(l,r)]
+        
+        total = sum(piles)
+        alice_score = dfs(0,len(piles) - 1)
+
+        return alice_score > alice_score - total // 2
+
+        
+
+        
